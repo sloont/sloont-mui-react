483d385327012e984b21dc8e24e1fd63478cf142
Sloont <colinabel93@gmail.com>
2021-06-07 12:38:09 -0400

    added navigation to the snips project from the projects page. netlify won't recognize the page if we manually add /snips to the url

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,32 @@
 import React from 'react';
 //import './pages.css';
+import { NavLink } from 'react-router-dom';
+import { Container, Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-import { Container, Typography } from '@material-ui/core';
+
+
+const useStyles = makeStyles({ ///Mostly temp stuff
+    tempContainer: {
+        margin: '6rem 6rem'
+        
+    },
+    navLinkButton: {
+        textDecoration: 'none',
+        textTransform: 'lowercase'
+    }
+});
 
 const Projects = () => {
+
+    const classes = useStyles();
+
     return (
-        <Container>
-            <Typography variant="h3">projects.</Typography>
+        <Container className={classes.tempContainer}>
+            <Typography variant="h3" style={{ marginBottom: '2rem'}}>projects.</Typography>
+            <Button variant="contained" color="secondary">
+                <NavLink to="/snips" className={classes.navLinkButton}>snips.</NavLink>
+            </Button>
         </Container>
     )
 }
