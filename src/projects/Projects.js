56e0aac241f6c1792186c5a49a795868c57281ac
Sloont <colinabel93@gmail.com>
2021-06-17 16:53:03 -0400

    no unused vars - useEffect in Projects.js

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import './resources/projects.css';
 import ProjectsObject from './resources/projectDetailsObject';
