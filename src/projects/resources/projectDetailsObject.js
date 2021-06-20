69f0fc2b8c8dcb9720440f9dbee5897f8e082de1
Sloont <colinabel93@gmail.com>
2021-06-19 20:57:28 -0400

    fixed the links to projects in projectDetailsObject.js

--- src/projects/resources/projectDetailsObject.js
+++ src/projects/resources/projectDetailsObject.js
@@ -19,7 +19,7 @@ const projectDetails = {
         title: 'snake lists',
         description: {
             text: 'The idea with this project was to create a program that could generate all possible unique schedules given a number of teams and weeks. Some customization for the teams was added to make it look friendlier. Schedule generator ended up being --and still is-- an ambitious work in progress. Teams can play a home and away fixture against every other team. Each added set of two teams increases the number of permuations exponentially, and this ended up limiting the number of teams the generator can handle. Currently schedule generator has a maximum of 8 teams due to the stress of the calculations required. If an odd number of teams is selected, byes are filled into the schedule. The produced schedules are valid and unique, defaulting to enough gameweeks that each team can play every other team both at home and away.',
-            link: 'https://sloont.github.io/snakelist',
+            link: 'https://sloont.github.io/snakelist/',
         },
         img: {
             src: 'https://i.imgur.com/xOXPA7l.png',
@@ -32,7 +32,7 @@ const projectDetails = {
         title: 'schedule generator',
         description: {
             text: 'The idea with this project was to create a program that could generate all possible unique schedules given a number of teams and weeks. Some customization for the teams was added to make it look friendlier. Schedule generator ended up being --and still is-- an ambitious work in progress. Teams can play a home and away fixture against every other team. Each added set of two teams increases the number of permuations exponentially, and this ended up limiting the number of teams the generator can handle. Currently schedule generator has a maximum of 8 teams due to the stress of the calculations required. If an odd number of teams is selected, byes are filled into the schedule. The produced schedules are valid and unique, defaulting to enough gameweeks that each team can play every other team both at home and away.',
-            link: 'https://sloont.github.io/schedule-generator',
+            link: 'https://sloont.github.io/ScheduleGenerator/',
         },
         img: {
             src: 'https://i.imgur.com/krcX45q.png',
@@ -45,7 +45,7 @@ const projectDetails = {
         title: 'divWatch',
         description: {
             text: 'When first learning asynchronous JavaScript, you are taught the set timeout function in the form of a stopwatch. I thought it could be interesting to take this example and instead of using text to display the clock, using html div elements like a digital clock. The JavaScript behind divwatch uses seven div elements for each digit of the clock and turns them off or on based on the number it is told to display. divwatch has start, stop, and reset functionality, just like your standard stopwatch.',
-            link: 'https://sloont.github.io/divWatch',
+            link: 'https://sloont.github.io/DivWatch/',
         },
         img: {
             src: 'https://i.imgur.com/WcGtE2y.png',
