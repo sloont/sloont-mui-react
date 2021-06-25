fc7000bc31be2618122f6c1d97f31406168fb497
Sloont <colinabel93@gmail.com>
2021-06-25 12:28:05 -0400

    we have a ball pit for the about page. color is a coin flip ternary to decide between one of the two theme colors

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,8 @@ import React, { useRef, useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core';
 import { Grid, Paper, Typography } from '@material-ui/core';
 import _debounce from 'lodash.debounce';
-import { checkMouseClick, applyForceWithClickAbout } from '../home/mousePhysics';
+import { checkMouseClick, applyForceWithClick } from '../home/mousePhysics';
+import { checkCollision, resolveCollision, adjustPositions } from '../home/physics';
 
 
 const useStyles = makeStyles({
@@ -20,7 +21,6 @@ const useStyles = makeStyles({
         height: '100%',
         cursor: 'pointer',
         maxHeight: 'calc(100vh - 8rem)'
-        
     },
     outerGrid: {
         justifyContent: 'center',
@@ -85,19 +85,33 @@ class Orb {
         //set deltas
         this.dx = 1.5 * this.speed;
         this.dy = 1 * this.speed;
-        this.gravity = [0, -0.05];
+        this.gravity = [0, -0.01];
+        console.log(typeof this.image);
     }
     draw() {
+        if (typeof this.image === 'string') {
+
+            this.context.fillStyle = this.image
+            this.context.beginPath();
+            this.context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+            this.context.fill();
+            this.context.closePath();
+
+
+        } else {
+
+            this.context.save();
+            this.context.beginPath();
+            /*test*/this.context.strokeStyle = "white";
+            /*test*/this.context.lineWidth = 5;
+            this.context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+            this.context.clip();
+            this.context.drawImage(this.image, (this.xpos - this.radius), (this.ypos - this.radius), this.radius *2, this.radius*2);
+            /*test*/this.context.stroke();
+            this.context.restore();
+
+        }
         
-        this.context.save();
-        this.context.beginPath();
-        /*test*/this.context.strokeStyle = "white";
-        /*test*/this.context.lineWidth = 5;
-        this.context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
-        this.context.clip();
-        this.context.drawImage(this.image, (this.xpos - this.radius), (this.ypos - this.radius), this.radius *2, this.radius*2);
-        /*test*/this.context.stroke();
-        this.context.restore();
     }
     update() {
         const xBound = this.canvas.width;
@@ -144,7 +158,34 @@ const About = () => {
     const [resized, setResized] = useState(false);
     const image = new Image();
     image.src = 'https://i.imgur.com/8mI7YJ2.jpg';
-    
+    const ballPit = [];
+
+    const generateOrbs = (canvas, context, gridItem) => {
+
+        if (ballPit.length < 1 || resized === true) {
+            
+            //for not spawning balls inside the walls
+            const randomNumber = (min, max) => {
+                return Math.random() * (max - min) + min;
+            }
+
+            let radius = (gridItem.clientHeight + gridItem.clientWidth) / 10
+            ballPit.push(new Orb(canvas.width / 2, canvas.height - radius/2, radius , 0, image, context, canvas));
+
+            for (let i = 0; i < 75; i++) {
+                const x = 1;
+                let skillSize = Math.sqrt(Math.sqrt(Math.sqrt( x * x * x * x * x * x * x * x))) * (gridItem.clientHeight + gridItem.clientWidth) / 150;
+                let randomx = randomNumber(skillSize, (canvas.width - skillSize));
+                let randomy = randomNumber(skillSize, (canvas.height - skillSize));
+                ballPit.push(new Orb(randomx, randomy, skillSize, 1, Math.floor(Math.random() * 2) > 0 ? '#75cff8' : '#ff905b', context, canvas));
+                
+            }
+
+        }
+    }
+
+
+
     useEffect(() => {
         
         const resizer = _debounce((event) => {
@@ -169,20 +210,21 @@ const About = () => {
         canvas.style.height = `${canvas.height}px`;
         
         let context = canvas.getContext('2d');
-        let radius = (gridItem.clientHeight + gridItem.clientWidth) / 10
-        let orb = new Orb(canvas.width / 2, canvas.height - radius/2, radius , 0, image, context, canvas);
+
+        generateOrbs(canvas, context, gridItem);
 
         const mouseClick = (event) => {
             const rect = canvas.getBoundingClientRect();
             mouse.x = event.clientX - rect.left;
             mouse.y = event.clientY - rect.top;
             console.log(mouse.x, mouse.y);
-    
-            checkMouseClick(orb, mouse);
-            if (checkMouseClick(orb, mouse)) {
-                applyForceWithClickAbout(orb, mouse, gridItem);
-                return;
-            }
+            ballPit.forEach(ballA => {
+                checkMouseClick(ballA, mouse);
+                if (checkMouseClick(ballA, mouse)) {
+                    applyForceWithClick(ballA, mouse, gridItem);
+                    return;
+                }
+            });
             
         }
         canvas.addEventListener("mousedown", mouseClick)
@@ -191,7 +233,19 @@ const About = () => {
         const animate = () => {
             context.clearRect(0, 0, canvas.width, canvas.height);
     
-            orb.update();
+            ballPit.forEach(ballA => {
+                ballA.update();
+    
+                ballPit.forEach(ballB => {
+                    if (ballA !== ballB) {
+                        const collision = checkCollision(ballA, ballB);
+                        if (collision[0]) {
+                            adjustPositions(ballA, ballB, collision[1]);
+                            resolveCollision(ballA, ballB);
+                        }
+                    }
+                });
+            });
             requestId = requestAnimationFrame(animate);
         }
     
@@ -214,17 +268,17 @@ const About = () => {
                 
                 <Grid item sm={12} className={classes.descriptionGridItem}><Paper className={classes.description}>
                     <div className={classes.innerDescription}><Typography gutterBottom variant='h4' align='center' className={classes.descriptionHeader}>what is <span style={{ color: '#75cff8' }}>sloont</span>?</Typography>
-                    <Typography>well, it's me. to be honest, i don't really know about the name. it's old and has stuck around for a long time.</Typography>
-                    <Typography>i'm <span style={{ color: '#ff905b' }}>colin.</span></Typography>
-                    <br />
-                    <Typography>i've been many things in life, from college dropout to barista, from unemployed to college graduate. i love learning new things, and if i could go to school forever, i probably would.</Typography>
-                    <Typography>i like soccer and hockey, number theory and precious metal refining, video games and cats. basically a whole handful of different things.</Typography>
-                    <br />
-                    <Typography>my education is in bio- and physical chemistry, but at some point i decided to go against it. i wanted to do other things.</Typography>
-                    <Typography>so here i am.</Typography>
-                    <br />
-                    <Typography>now, web development is the name of the game. for a few years i've been grinding my axe, and in the last six months i hit that magic moment where you don't feel like an impostor any more.</Typography>
-                    <Typography>...and i started creating. now i don't know how to stop.</Typography>
+                        <Typography variant='subtitle1'>well, it's me. to be honest, i don't really know about the name. it's old and has stuck around for a long time.</Typography>
+                        <Typography variant='subtitle1'>i'm <span style={{ color: '#ff905b' }}>colin.</span></Typography>
+                        <br />
+                        <Typography variant='subtitle1'>i've been many things in life, from college dropout to barista, from unemployed to college graduate. i love learning new things, and if i could go to school forever, i probably would.</Typography>
+                        <Typography variant='subtitle1'>i like soccer and hockey, number theory and precious metal refining, video games and cats. basically a whole handful of different things.</Typography>
+                        <br />
+                        <Typography variant='subtitle1'>my education is in bio- and physical chemistry, but at some point i decided to go against it. i wanted to do other things.</Typography>
+                        <Typography variant='subtitle1'>so here i am.</Typography>
+                        <br />
+                        <Typography variant='subtitle1'>now, web development is the name of the game. for a few years i've been grinding my axe, and in the last six months i hit that magic moment where you don't feel like an impostor any more.</Typography>
+                        <Typography variant='subtitle1'>...and i started creating. now i don't know how to stop.</Typography>
                 </div></Paper></Grid>
                 <Grid item sm={12} className={classes.linksGridItem}><Paper className={classes.links}></Paper></Grid>
                 
